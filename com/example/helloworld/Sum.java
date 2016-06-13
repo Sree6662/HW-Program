d6096c7a436a04c6e2fde0bb1fdc26fcd897daf9
Sreeraj Rajendran <Sreeraj Rajendran>
2016-06-13 11:36:32 +0530

    Sum class commit

---
+++ com/example/helloworld/Sum.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by A-6662 on 13-06-2016.
+ */
+public class Sum {
+    public static void main(String[] args) {
+        System.out.println("Sum of 5+3 is :"+(5+3));
+    }
+}
