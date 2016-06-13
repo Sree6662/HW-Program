6eb496c6460d3d450ac773a131944772f654a3cd
Sreeraj Rajendran <Sreeraj Rajendran>
2016-06-13 11:24:53 +0530

    First commit

---
+++ com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by A-6662 on 10-06-2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World !!");
+    }
+}
